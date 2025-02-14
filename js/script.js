9afac99b0682d1c4544b314f46f310eacfcc360e
Ashfaque Hossain Abir <ashfaquehossainabirr@gmail.com>
2025-02-14 19:09:18 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -52,23 +52,6 @@ $(function () {
   });
 
   /*=========================================================================
-     One Page Scroll with jQuery
-     =========================================================================*/
-  $('a[href^="#"]:not([href="#"]').on("click", function (event) {
-    var $anchor = $(this);
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $($anchor.attr("href")).offset().top,
-        },
-        800,
-        "easeInOutQuad"
-      );
-    event.preventDefault();
-  });
-
-  /*=========================================================================
      Spacer with Data Attribute
      =========================================================================*/
   var list = document.getElementsByClassName("spacer");
@@ -87,28 +70,6 @@ $(function () {
     var color = list[i].getAttribute("data-color");
     list[i].style.backgroundColor = "" + color + "";
   }
-
-  /*=========================================================================
-            Scroll to Top
-    =========================================================================*/
-  $(window).scroll(function () {
-    if ($(this).scrollTop() >= 350) {
-      // If page is scrolled more than 50px
-      $("#return-to-top").fadeIn(200); // Fade in the arrow
-    } else {
-      $("#return-to-top").fadeOut(200); // Else fade out the arrow
-    }
-  });
-  $("#return-to-top").on("click", function (event) {
-    // When arrow is clicked
-    event.preventDefault();
-    $("body,html").animate(
-      {
-        scrollTop: 0, // Scroll to top of body
-      },
-      400
-    );
-  });
 });
 
 
