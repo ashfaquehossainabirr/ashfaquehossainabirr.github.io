f16519aaf95d145e68e23ed741e3de9159a41d9f
Ashfaque Hossain Abir <ashfaquehossainabirr@gmail.com>
2025-02-18 20:39:36 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -178,6 +178,25 @@ yearText.innerHTML = year;
 
 
 // ==========================================================
+// -- Active Navlink OnScroll
+// ==========================================================
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-menu-section ul li');
+
+function changeActiveLink() {
+    let index = sections.length;
+
+    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+    navLinks.forEach((link) => link.classList.remove('active'));
+    navLinks[index].classList.add('active');
+}
+
+window.addEventListener('scroll', changeActiveLink);
+
+
+// ==========================================================
 // -- Work Section Data Inject
 // ==========================================================
 
